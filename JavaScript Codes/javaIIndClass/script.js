34a12ffff1caf7275106799b2ef499fe22549975
MansoorAhmedk <abbasimansoor97@gmail.com>
2024-03-28 16:23:07 +0500

    javascript files added

---
+++ JavaScript Codes/javaIIndClass/script.js
@@ -0,0 +1,93 @@
+// let a = 2;
+// let b = String(a);
+// console.log("Data Type of a is "+ String(a));
+// console.log("Data Type of a is "+ b);
+
+
+
+let a = 7;
+let b = 5;
+
+let c = 3;
+let d = 3;
+
+// let sum = a+b;
+// let sub = a - b
+// let mult = a*b;
+// let divide = a/b;
+// let mod = a % b
+
+// let exp = c**b
+
+// let inc = ++a
+// let dec = --a
+
+// console.log(dec);
+// console.log(inc);
+
+
+// a **= 10
+// console.log(a);
+
+// console.log(a >= b);
+
+// console.log(a!=a);
+
+// if (a == 7) {
+//     console.log("You are a good student.");
+// }
+// else{
+//     console.log("You are not a good student");
+// }
+
+
+// let score = prompt("Enter your score: ");
+// if(score <=100){
+//     console.log(" you are a Topper.");
+// }
+// else if (score >= 90 ){
+//     console.log(" you secred A grade.");
+   
+// }
+// else if (score >= 80 ) {
+//     console.log(" you secred B grade.");
+// }else if (score >= 70) {
+//     console.log(" you secred C grade.");
+// }else if (score >= 60) {
+//     console.log(" you secred D grade.");
+// }else if (score >= 50) {
+//     console.log(" you secred F grade.");
+// }
+// else{
+//     console.log(" you are considered to apply in next year.");
+// }
+
+
+
+// let password = prompt("Enter your Password: ")
+
+// if(password == "Mansoor"){
+//     console.log("Welcome Mansoor!");
+// }
+// else{
+//     console.log("Invalid Credentials.");
+// }
+
+let password = prompt("Enter your Password!!");
+
+switch (password) {
+    case "Rafiq":
+        console.log("Welcome Rafiq!");
+        break;
+    case "Mansoor":
+        console.log("Welcome Mansoor!");x
+        break;
+    case "Shahzad":
+        console.log("Welcome Shahzad!");
+        break;
+
+    default:
+        console.log("Incorrect Password!");
+        break;
+}
+
---
+++ JavaScript Codes/javascript/main.js
@@ -0,0 +1,48 @@
+// let firstName = "Mansoor";
+// let lastName = "Ahmed";
+// let profession = "Software Engineer";
+
+
+// console.log("Hi I am " + firstName+ " " + lastName + " and I work as a "+ profession + "at Btyesiwe type - II." );
+
+// // alert(2 + 2)
+
+// // confirm("Do you want to proceed?")
+
+// let a = document.getElementById('num1').value;
+// let b = document.getElementById('num2').value;
+
+// let sum = a =b;
+
+// document.getElementById('result').textContent = d;
+
+// function addNumbers() {
+//     // Get the input values
+//     var num1 = parseFloat(document.getElementById('num1').value);
+//     var num2 = parseFloat(document.getElementById('num2').value);
+
+//     // Check if the input is valid
+//     if (isNaN(num1) || isNaN(num2)) {
+//         document.getElementById('result').textContent = 'Please enter valid numbers';
+//         return;
+//     }
+
+//     // Add the numbers
+//     var sum = num1 + num2;
+
+//     // Display the result
+//     document.getElementById('result').textContent = 'Result: ' + sum;
+// }
+
+
+let a;
+document.getElementById('header').innerText= "Kiya hal hain bhai"
+a = document.getElementById('header')
+console.log(a);
+
+
+document.write("Hi Kiya hain bhai jan")
+let numb;
+numb = prompt("Enter any number?")
+
+document.write("The value you entered is " + numb)
